41f37f9cc6bd1e2bb9b3e07f8f9b4838de663b7f
Seth <SethMessenger@126.com>
2018-03-12 11:02:55 +0800

    project init

---
+++ src/main/java/com/seth/model/SethTransaction.java
@@ -0,0 +1,15 @@
+package com.seth.model;
+
+/**
+ * @author xunbo.xu
+ * @desc    区块链交易对象
+ * @date 18/3/12
+ */
+public class SethTransaction {
+
+    /** 发送给那个用户钱包 **/
+    private String toUser;
+
+
+
+}
